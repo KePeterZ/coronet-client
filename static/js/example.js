beb5a1f48b787ca562405aca0c88c41b4927557a
mirusz9 <adam.sztano@gmail.com>
2020-06-04 11:24:04 +0200

    with news

--- static/js/example.js
+++ static/js/example.js
@@ -1,4 +1,39 @@
 const counterText = document.getElementById("counterText");
+const newsTag = document.getElementById("news");
+
+const theJson = {
+  "news": [
+    {
+      "title": "adam buta",
+			"timetoread": "5 perc",
+			"url": "http://localhost:3000"
+		},
+		{
+      "title": "peti is buta",
+			"timetoread": "5 perc",
+			"url": "http://localhost:3000"
+    }
+  ]
+}
+
+theJson.news.forEach((news) => {
+	currentNew = document.createElement("div");
+	title = document.createElement("h1");
+	title.appendChild(document.createTextNode(news.title));
+	currentNew.appendChild(title);
+	timetoread = document.createElement("h2");
+	timetoread.appendChild(document.createTextNode(news.timetoread + " elolvasni"));
+	currentNew.appendChild(timetoread);
+	link = document.createElement("a");
+	link.appendChild(document.createTextNode(news.url));
+	link.setAttribute("href", news.url);
+	currentNew.appendChild(link);
+	currentNew.id = "caption";
+	currentNew.style.paddingTop = "40px"
+
+	newsTag.appendChild(currentNew);
+});
+
 
 function updateTimer() {
 	const minutesLeft = 60 - (new Date(window.performance.now()).getMinutes())
