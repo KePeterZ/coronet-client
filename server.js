a8bb6f68d61ba4441b459d8c9576227943f27033
mirusz9 <adam.sztano@gmail.com>
2020-06-04 10:54:51 +0200

    first commit

--- server.js
+++ server.js
@@ -2,25 +2,21 @@ let express = require('express');
 let app = express();
 
 // Setup port
-thePort = process.env.PORT || 2020
+const thePort = process.env.PORT || 3000
 
 // Setup ejs and static content
 app.set('view engine', 'ejs');
 app.use(express.static('static'))
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {cucc:"1"});
 });
 
-app.post('/post', (req, res) => {
-  res.end(res.toString())
-  console.log(res.toString())
-})
 
 app.use((req, res) => {
   res.redirect("/");
 });
 
 app.listen(thePort, () => {
-  console.log('Example app listening on port ' + thePort)
+  console.log('Listening on port ' + thePort)
 })
--- static/js/example.js
+++ static/js/example.js
@@ -1,1 +1,15 @@
-document.body.innerHTML += "Hello there!"
+const counterText = document.getElementById("counterText");
+
+function updateTimer() {
+	const minutesLeft = 60 - (new Date(window.performance.now()).getMinutes())
+	console.log(minutesLeft)
+	if (minutesLeft <= 0) {
+		counterText.innerHTML = "Most"
+		alert("Moss kezet!");
+	} else {
+		counterText.innerHTML = `${minutesLeft} perc`;
+	}
+	
+}
+
+setInterval(updateTimer, 1000);
